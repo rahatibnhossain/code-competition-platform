6c0e02686fba1c12b1cbd2f5b7d228e691d997c6
rahat <rahatibnhossain@gmail.com>
2025-10-24 17:16:48 +0600

    [gateway] init

--- problem.service/src/main/java/com/boj/problem/service/repository/ProblemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.boj.problem.service.repository;
-
-import com.boj.problem.service.entity.Problem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProblemRepository extends JpaRepository<Problem, Long> {
-}
--- submission.service/src/main/java/com/boj/submission/service/repository/SubmissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.boj.submission.service.repository;
-
-import com.boj.submission.service.entity.Submission;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SubmissionRepository extends JpaRepository<Submission, Long> {
-}
